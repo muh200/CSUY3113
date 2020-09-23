bf5e16fa1b2add99ede527bfa2ae3788bbb1cf29
Muhannad <>
2020-09-23 11:14:08 -0400

    Made a function to draw rectangles

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -14,6 +14,12 @@
 #include <cstdlib>
 #include <ctime>
 
+struct Rectangle {
+	const glm::vec3 topLeft;
+	const glm::vec3 bottomRight;
+	glm::vec3 position;
+};
+
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
@@ -21,18 +27,43 @@ ShaderProgram program;
 glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
 
 float rightPaddleDirection = 0.0f;
-float rightPaddlePosition = 0.0f;
+Rectangle rightPaddle{glm::vec3(-0.25, 1, 0), glm::vec3(0.25, -1, 0), glm::vec3(4.75, 0, 0)};
 
 float leftPaddleDirection = 0.0f;
-float leftPaddlePosition = 0.0f;
+Rectangle leftPaddle{glm::vec3(-0.25, 1, 0), glm::vec3(0.25, -1, 0), glm::vec3(-4.75, 0, 0)};
 
-glm::vec3 ballPosition = glm::vec3(0, 0, 0);
 glm::vec3 ballDirection = glm::vec3(0, 0, 0);
+Rectangle ball{glm::vec3(-0.25, 0.25, 0), glm::vec3(0.25, -0.25, 0), glm::vec3(0, 0, 0)};
 
 float lastTicks = 0.0f;
 
 bool randomBool() {
 	return std::rand() < (RAND_MAX / 2);
+}
+
+void drawRectangle(const Rectangle& rect) {
+	modelMatrix = glm::translate(glm::mat4(1.0f), rect.position);
+	program.SetModelMatrix(modelMatrix);
+
+	float vertices[] = {
+		rect.topLeft[0],
+		rect.bottomRight[1],
+		rect.bottomRight[0],
+		rect.bottomRight[1],
+		rect.bottomRight[0],
+		rect.topLeft[1],
+		rect.topLeft[0],
+		rect.bottomRight[1],
+		rect.bottomRight[0],
+		rect.topLeft[1],
+		rect.topLeft[0],
+		rect.topLeft[1],
+	};
+
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+	glEnableVertexAttribArray(program.positionAttribute);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDisableVertexAttribArray(program.positionAttribute);
 }
 
 void Initialize() {
@@ -108,30 +139,30 @@ void Update() {
 
 	const float paddleSpeed = 5.0f;
 
-	rightPaddlePosition += rightPaddleDirection * paddleSpeed * deltaTime;
+	rightPaddle.position[1] += rightPaddleDirection * paddleSpeed * deltaTime;
 
-	if (rightPaddlePosition > 2.75) {
-		rightPaddlePosition = 2.75;
-	} else if (rightPaddlePosition < -2.75) {
-		rightPaddlePosition = -2.75;
+	if (rightPaddle.position[1] > 2.75) {
+		rightPaddle.position[1] = 2.75;
+	} else if (rightPaddle.position[1] < -2.75) {
+		rightPaddle.position[1] = -2.75;
 	}
 
-	leftPaddlePosition += leftPaddleDirection * paddleSpeed * deltaTime;
+	leftPaddle.position[1] += leftPaddleDirection * paddleSpeed * deltaTime;
 
-	if (leftPaddlePosition > 2.75) {
-		leftPaddlePosition = 2.75;
-	} else if (leftPaddlePosition < -2.75) {
-		leftPaddlePosition = -2.75;
+	if (leftPaddle.position[1] > 2.75) {
+		leftPaddle.position[1] = 2.75;
+	} else if (leftPaddle.position[1] < -2.75) {
+		leftPaddle.position[1] = -2.75;
 	}
 
 	const float ballSpeed = 5.0f;
-	ballPosition += ballDirection * ballSpeed * deltaTime;
+	ball.position += ballDirection * ballSpeed * deltaTime;
 
-	if (ballPosition[1] < -3.5) {
-		ballPosition[1] = -3.5;
+	if (ball.position[1] < -3.5) {
+		ball.position[1] = -3.5;
 		ballDirection[1] *= -1;
-	} else if (ballPosition[1] > 3.5) {
-		ballPosition[1] = 3.5;
+	} else if (ball.position[1] > 3.5) {
+		ball.position[1] = 3.5;
 		ballDirection[1] *= -1;
 	}
 }
@@ -139,40 +170,9 @@ void Update() {
 void Render() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	modelMatrix = glm::mat4(1.0f);
-	modelMatrix = glm::translate(modelMatrix, glm::vec3(4.75f, rightPaddlePosition, 0.0f));
-
-	program.SetModelMatrix(modelMatrix);
-
-	float paddleVertices[] = { -0.25, -1.0, 0.25, -1.0, 0.25, 1.0, -0.25, -1.0, 0.25, 1.0, -0.25, 1.0 };
-
-	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, paddleVertices);
-	glEnableVertexAttribArray(program.positionAttribute);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDisableVertexAttribArray(program.positionAttribute);
-
-
-	modelMatrix = glm::mat4(1.0f);
-	modelMatrix = glm::translate(modelMatrix, glm::vec3(-4.75f, leftPaddlePosition, 0.0f));
-
-	program.SetModelMatrix(modelMatrix);
-
-	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, paddleVertices);
-	glEnableVertexAttribArray(program.positionAttribute);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDisableVertexAttribArray(program.positionAttribute);
-
-	float ballVertices[] = { -0.25, -0.25, 0.25, -0.25, 0.25, 0.25, -0.25, -0.25, 0.25, 0.25, -0.25, 0.25 };
-
-	modelMatrix = glm::mat4(1.0f);
-	modelMatrix = glm::translate(modelMatrix, ballPosition);
-
-	program.SetModelMatrix(modelMatrix);
-
-	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ballVertices);
-	glEnableVertexAttribArray(program.positionAttribute);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDisableVertexAttribArray(program.positionAttribute);
+	drawRectangle(rightPaddle);
+	drawRectangle(leftPaddle);
+	drawRectangle(ball);
 
 	SDL_GL_SwapWindow(displayWindow);
 }
